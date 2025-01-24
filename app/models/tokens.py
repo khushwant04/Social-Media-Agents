8d0484dafa09c9aa0c423affaf40b81914637494
khushwant04 <khushwantsanwalot2004@outlook.com>
2025-01-24 20:13:36 +0530

    commit

--- app/models/tokens.py
+++
@@ -1,9 +0,0 @@
-# Define the Token model
-from sqlalchemy import Column, String, Text
-
-
-class Token(Base):
-    __tablename__ = "tokens"
-    user_id = Column(String, primary_key=True, index=True)
-    access_token = Column(Text, nullable=False)
-    linkedin_urn = Column(String, nullable=False)  # Added field
